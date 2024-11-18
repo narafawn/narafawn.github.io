2e868f9d803bad4169e896022d2979d06d425687
GitHub30 <funaox@gmail.com>
2024-11-18 15:00:53 +0000

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container>
-        <v-btn href="password-generator" class="mr-4" variant="outlined" color="primary">Password Generator</v-btn>
-        <v-btn href="uuid-generator" class="mr-4" variant="outlined" color="primary">UUID Generator</v-btn>
-        <v-btn href="gz-extractor" class="mr-4" variant="outlined" color="primary">file.gz Extractor</v-btn>
-        <v-btn href="totp-generator" class="mr-4" variant="outlined" color="primary">TOTP Generator</v-btn>
-        <v-btn href="video-frame-picker" class="mr-4" variant="outlined" color="primary">Video Frame Picker</v-btn>
+        <v-btn href="password-generator" class="ma-2" variant="outlined" color="primary">Password Generator</v-btn>
+        <v-btn href="uuid-generator" class="ma-2" variant="outlined" color="primary">UUID Generator</v-btn>
+        <v-btn href="gz-extractor" class="ma-2" variant="outlined" color="primary">file.gz Extractor</v-btn>
+        <v-btn href="totp-generator" class="ma-2" variant="outlined" color="primary">TOTP Generator</v-btn>
+        <v-btn href="video-frame-picker" class="ma-2" variant="outlined" color="primary">Video Frame Picker</v-btn>
     </v-container>
 </template>
 
