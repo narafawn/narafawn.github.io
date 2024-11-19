ece9fc7afa02f32eb636e489b182d41723d04f8b
GitHub30 <funaox@gmail.com>
2024-11-19 20:02:39 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module']
+  modules: ['vuetify-nuxt-module'],
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    }
+  }
 })
