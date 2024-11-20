7e2191b64350882e7bcea606d7de938444ed80d7
GitHub30 <funaox@gmail.com>
2024-11-20 12:43:40 +0900

    Update unserialize-php.vue

--- pages/unserialize-php.vue
+++ pages/unserialize-php.vue
@@ -4,18 +4,19 @@ import { ref } from 'vue'
 const input = ref('a:2:{i:0;s:12:"Sample array";i:1;a:2:{i:0;s:5:"Apple";i:1;s:6:"Orange";}}')
 const output = ref('')
 
+let php = null
 async function handleInput() {
-    const { PhpWeb } = await import('https://cdn.jsdelivr.net/npm/php-wasm/PhpWeb.mjs');
-    const php = new PhpWeb
     output.value = ''
-    php.addEventListener('output', (event) => {
-        output.value += event.detail;
-    })
     const encoded = btoa(input.value)
     await php.run(`<?php var_export(unserialize(base64_decode('${encoded}')));`)
 }
 
-onMounted(() => {
+onMounted(async () => {
+    const { PhpWeb } = await import('https://cdn.jsdelivr.net/npm/php-wasm/PhpWeb.mjs');
+    php = new PhpWeb
+    php.addEventListener('output', (event) => {
+        output.value += event.detail;
+    })
     handleInput()
 })
 
