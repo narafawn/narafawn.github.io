c664a188c5a42e65f34454861dd5317cffc34856
GitHub30 <funaox@gmail.com>
2024-12-05 14:17:13 +0000

    add

--- pages/wake-lock.vue
+++ pages/wake-lock.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 
 const message = ref('Wake lock is not supported by this browser.')
-const elapsed = ref(0)
+const elapsed = ref('00:00')
 let start = 0
 let intervalId
 
@@ -18,7 +18,10 @@ async function requestWakeLock() {
         start = Date.now()
         intervalId = setInterval(() => {
             const s = Math.floor((Date.now() - start) / 1000)
-            elapsed.value = `${Math.floor(s / 60).toString().padStart(2, 0)}:${(s % 60).toString().padStart(2, 0)}`
+            const ss = (s % 60).toString().padStart(2, 0)
+            const m = (Math.floor(s / 60) % 60).toString().padStart(2, 0)
+            const h = Math.floor(s / 60 / 60) > 0 ? Math.floor(s / 60 / 60).toString().padStart(2, 0) + ':' : ''
+            elapsed.value = `${h}${m}:${ss}`
         }, 1000)
     } catch (error) {
         message.value = `${error.name}, ${error.message}`
